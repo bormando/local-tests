5fd0909337f2ecae84c9b4a32f9b02b81f727153
Dmitrii Bormotov <squier7@gmail.com>
2023-04-29 10:39:49 +0700

    Add negative scenarios

--- cypress/e2e/auth.cy.js
+++ cypress/e2e/auth.cy.js
@@ -1,9 +1,75 @@
 describe('Auth', () => {
   it('Sign in with valid credentials', () => {
     cy.visit('https://localcoding.us/user/login')
+
     cy.get('#normal_login_email').type('test@example.com')
     cy.get('#normal_login_password').type('Qwerty!23')
     cy.get('.login-form-button').click()
+
     cy.get('.ant-avatar-square').should('be.visible')
+  })
+
+  it('Sign in with incorrect credentials', () => {
+    cy.visit('https://localcoding.us/user/login')
+
+    cy.get('#normal_login_email').type('test@example.com')
+    cy.get('#normal_login_password').type('123456')
+    cy.get('.login-form-button').click()
+
+    cy.get('.ant-notification-notice-message')
+      .should('have.text', 'Auth failed')
+      .should('be.visible')
+  })
+
+  it('Sign in form validation', () => {
+    cy.visit('https://localcoding.us/user/login')
+
+    cy.get('#normal_login_email').should('have.value', '')
+    cy.get('#normal_login_password').should('have.value', '')
+    cy.get('.login-form-button').should('be.disabled')
+
+    cy.get('#normal_login_password').type('test')
+    cy.get('#normal_login_password_help').should('not.exist')
+    cy.get('.login-form-button').should('be.disabled')
+
+    cy.get('#normal_login_email').type('test')
+    cy.get('#normal_login_email_help')
+      .should('have.text', `'email' is not a valid email`)
+      .should('be.visible')
+    cy.get('.login-form-button').should('be.disabled')
+
+    cy.get('#normal_login_email').type('@')
+    cy.get('#normal_login_email_help')
+      .should('have.text', `'email' is not a valid email`)
+      .should('be.visible')
+    cy.get('.login-form-button').should('be.disabled')
+
+    cy.get('#normal_login_email').type('example')
+    cy.get('#normal_login_email_help')
+      .should('have.text', `'email' is not a valid email`)
+      .should('be.visible')
+    cy.get('.login-form-button').should('be.disabled')
+
+    cy.get('#normal_login_email').type('.')
+    cy.get('#normal_login_email_help')
+      .should('have.text', `'email' is not a valid email`)
+      .should('be.visible')
+    cy.get('.login-form-button').should('be.disabled')
+
+    cy.get('#normal_login_email').type('com')
+    cy.get('#normal_login_email_help').should('not.exist')
+    cy.get('.login-form-button').should('be.enabled')
+
+    cy.get('#normal_login_email').clear()
+    cy.get('#normal_login_email_help')
+      .should('have.text', 'Required')
+      .should('be.visible')
+    cy.get('.login-form-button').should('be.disabled')
+
+    cy.get('#normal_login_password').clear()
+    cy.get('#normal_login_password_help')
+      .should('have.text', 'Required')
+      .should('be.visible')
+    cy.get('.login-form-button').should('be.disabled')
   })
 })
