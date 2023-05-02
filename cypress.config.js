73658df1e4a3be5fc6dc8a427c52cac0478a6e35
Dmitrii Bormotov <squier7@gmail.com>
2023-05-02 20:39:00 +0700

    Exclude example specs from test runs

--- cypress.config.js
+++ cypress.config.js
@@ -3,5 +3,9 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     watchForFileChanges: false,
+    excludeSpecPattern: [
+      'cypress/e2e/1-getting-started/*',
+      'cypress/e2e/2-advanced-examples/*',
+    ],
   },
 });
