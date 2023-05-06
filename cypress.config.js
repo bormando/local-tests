2a5a996f4c941cf121600734864585915c26a972
Dmitrii Bormotov <squier7@gmail.com>
2023-05-06 11:31:55 +0700

    Add base url Apply formatter

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,9 @@
-const { defineConfig } = require("cypress");
+const {defineConfig} = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: 'https://localcoding.us',
     watchForFileChanges: false,
-    excludeSpecPattern: [
-      'cypress/e2e/1-getting-started/*',
-      'cypress/e2e/2-advanced-examples/*',
-    ],
+    excludeSpecPattern: ['cypress/e2e/1-getting-started/*', 'cypress/e2e/2-advanced-examples/*'],
   },
-});
+})
