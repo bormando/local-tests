94a1276536ddbf8f30690b9b26d91d3263342eb3
Dmitrii Bormotov <squier7@gmail.com>
2023-05-23 19:26:11 +0700

    Add Project ID for Cypress Cloud

--- cypress.config.js
+++ cypress.config.js
@@ -7,5 +7,6 @@ module.exports = defineConfig({
     excludeSpecPattern: ['cypress/e2e/1-getting-started/*', 'cypress/e2e/2-advanced-examples/*'],
     viewportWidth: 1200,
     viewportHeight: 700,
+    projectId: "jyazyh",
   },
 })
